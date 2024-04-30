d3c6cba57b1ed8cc8232fe70d57aac266f55bfc8
Dipa Galani <dg180901@gmail.com>
2024-04-30 10:06:19 +0700

    added functionality

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const voicesSelect = document.getElementById("voices");
 const textarea = document.getElementById("text");
 const readBtn = document.getElementById("read");
 const toggleBtn = document.getElementById("toggle");
-const close = document.getElementById("close");
+const closeBtn = document.getElementById("close");
 
 const data = [
   {
@@ -68,8 +68,71 @@ const createBox = (item) => {
   `;
 
   // @todo - speak event
+  box.addEventListener("click", () => {
+    setTextMessage(text);
+    speakText();
+
+    // Add an active effect
+    box.classList.add("active");
+    setTimeout(() => {
+      box.classList.remove("active");
+    }, 800);
+  });
 
   main.appendChild(box);
 };
 
+// Init speech synth
+const message = new SpeechSynthesisUtterance();
+
+// Store voices
+let voices = [];
+
+const getVoices = () => {
+  voices = speechSynthesis.getVoices();
+  voices.forEach((voice) => {
+    const option = document.createElement("option");
+    option.setAttribute("value", voice.name);
+    option.textContent = `${voice.name} - ${voice.lang}`;
+
+    voicesSelect.appendChild(option);
+  });
+};
+
+// Set text
+const setTextMessage = (text) => {
+  message.text = text;
+};
+
+// speak text
+const speakText = () => {
+  speechSynthesis.speak(message);
+};
+
+// change voice
+const setVoice = (e) => {
+  message.voice = voices.find((voice) => voice.name === e.target.value);
+};
+
+// Voices changed
+speechSynthesis.addEventListener("voiceschanged", getVoices);
+
 data.forEach(createBox);
+
+// Event Listeners
+toggleBtn.addEventListener("click", () =>
+  document.getElementById("text-box").classList.toggle("show")
+);
+
+closeBtn.addEventListener("click", () =>
+  document.getElementById("text-box").classList.remove("show")
+);
+
+voicesSelect.addEventListener("change", setVoice);
+
+readBtn.addEventListener("click", () => {
+  setTextMessage(textarea.value);
+  speakText();
+});
+
+getVoices();
